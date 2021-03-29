dbf2bda27fbbcb638332f12fa989dc5411bd6e5d
Marcos Carbonell <markcarbonell2013@gmail.com>
2021-03-29 15:49:55 +0200

    - // Update last part of carbo_agent

--- agent_code/carbo_agent/callbacks.py
+++ agent_code/carbo_agent/callbacks.py
@@ -9,12 +9,15 @@ import numpy as np
 
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
+t = 10
+t0 = 4
+
 def setup(self):
     pass
 
 def act(self, game_state: dict) -> str:
     game_state = preprocess_game_state(game_state)
-    action = decide(game_state)
+    action = decide(game_state, t, t0)
     return action
 
 def preprocess_game_state(game_state: dict) -> dict:
